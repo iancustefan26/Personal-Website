7b8edeb2040de3e932e3a3f12ad6d496979b40c9
iancustefan26 <stefaniancu46@gmail.com>
2023-11-02 11:42:21 +0200

    Some bakcend configuration for form submition

--- formScript.js
+++ formScript.js
@@ -15,16 +15,20 @@ formButton.addEventListener('click', function(e){
         user_email: email,
         message: message
     };
-     
-    emailjs.send('service_sc4gm47', 'template_ubfch6p', templateParams)
-        .then(function(response) {
-           console.log('SUCCESS!', response.status, response.text);
-           alert('Message was sent successfully!');
-           nameInput.value = '';
-           emailInput.value = '';
-           messageInput.value = '';
-        }, function(error) {
-           console.log('FAILED...', error);
-           alert('There was an error sending your message!' + error);
-        });
+    if(name != '' && email != '' && message != '')
+        emailjs.send('service_sc4gm47', 'template_ubfch6p', templateParams)
+            .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+            alert('Message was sent successfully!');
+            nameInput.value = '';
+            emailInput.value = '';
+            messageInput.value = '';
+            }, function(error) {
+            console.log('FAILED...', error);
+            alert('There was an error sending your message!' + error);
+            });
+    else{
+        alert('All fields need to be filled!');
+        return;
+    }
 });
