35cd6aed099b33e6f92b0e737adbce6d79299419
iancustefan26 <stefaniancu46@gmail.com>
2023-11-02 21:06:49 +0200

    SOme backend email modification for validation

--- formScript.js
+++ formScript.js
@@ -15,18 +15,24 @@ formButton.addEventListener('click', function(e){
         user_email: email,
         message: message
     };
-    if(name != '' && email != '' && message != '')
-        emailjs.send('service_sc4gm47', 'template_ubfch6p', templateParams)
-            .then(function(response) {
-            console.log('SUCCESS!', response.status, response.text);
-            alert('Message was sent successfully!');
-            nameInput.value = '';
-            emailInput.value = '';
-            messageInput.value = '';
-            }, function(error) {
-            console.log('FAILED...', error);
-            alert('There was an error sending your message!' + error);
-            });
+    if(name != '' && email != '' && message != ''){
+        if (email.indexOf('.') !== -1 && email.indexOf('@') !== -1)
+            emailjs.send('service_sc4gm47', 'template_ubfch6p', templateParams)
+                .then(function(response) {
+                console.log('SUCCESS!', response.status, response.text);
+                alert('Message was sent successfully!');
+                nameInput.value = '';
+                emailInput.value = '';
+                messageInput.value = '';
+                }, function(error) {
+                console.log('FAILED...', error);
+                alert('There was an error sending your message!' + error);
+                });
+        else if(email.indexOf('.') == -1 || email.indexOf('@') == -1){
+            alert('Email has to be valid!');
+            return;
+        }
+    }
     else{
         alert('All fields need to be filled!');
         return;
